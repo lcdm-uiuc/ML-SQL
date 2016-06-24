f5d1f8f89bc76c2a6b8e8527fcbfa40056d37a74
Neeraj Asthana <neeasthana@gmail.com>
2016-06-24 16:02:37 +0000

    completed shell. need to test

--- ML-SQL/Language/mlsql.py
+++ ML-SQL/Language/mlsql.py
@@ -4,9 +4,19 @@ sys.path.insert(0, 'parser/')
 
 from parser import parser
 
+def repl(parser):
+	while True:
+		command = input(">")
+		if command.lower() == "exit":
+			print("exiting")
+		else:
+			result = parser.parseString(command)
+			print(result)
+
 if __name__ == "__main__":
 	#initialize the parser
+	print("Initializing Parser...")
 	myparser = parser()
-
-
-
+	
+	print("ML-SQL Parser is ready for use")
+	repl(parser)
