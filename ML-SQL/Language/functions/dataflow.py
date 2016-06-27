ea1c94d0c270de9749ddb8a445e6376cd418bbc3
Neeraj Asthana <nasthan2@illinois.edu>
2016-06-27 00:07:53 -0500

    added some logic for read keyword and created dataflow

---
+++ ML-SQL/Language/functions/dataflow.py
@@ -0,0 +1,6 @@
+"""
+Processes input after it has been parsed. Performs the dataflow for input. 
+"""
+
+def handle(parsing):
+	pass
---
+++ ML-SQL/Language/functions/keywords/read_functions.py
@@ -0,0 +1,8 @@
+"""
+Performs logic to handle the read keyword from ML-SQL language
+"""
+
+def handle(userfile, separator, header):
+	from pandas import read_csv
+	df = read_csv(userfile, sep = separator, header = header)
+	return (df)
