b05625ddf5bd618965f279bfb67cc3578103f281
Neeraj Asthana <nasthan2@illinois.edu>
2016-06-26 23:58:37 -0500

    added execute function to mlsql main file

--- ML-SQL/Language/mlsql.py
+++ ML-SQL/Language/mlsql.py
@@ -4,8 +4,22 @@ sys.path.insert(0, 'parser/')
 
 from parser import parser
 
+def execute(command):
+	"""
+	Used to parse and execute an ML-SQL command supplied by the user. 
+	This function is meant to exported for use in other modules.
+	"""
+	myparser = parser()
+	result = myparser.parseString(command)
+	print(result)
+
+
 def repl(parser):
-	#initialize the parser
+	"""
+	Used to create a read-evaluate-print-loop sequence for the ML-SQL langauge.
+	This function will continually parse and execute user input from command
+	line until the word exit is typed. 
+	"""
 	print("Initializing Parser...")
 	myparser = parser()
 	
