1f234c91e3ca6051028eef93e0c54b6cfe915a5e
Neeraj Asthana <nasthan2@illinois.edu>
2016-06-27 14:15:55 -0500

    setting up mlsql language for packaging properly

---
+++ language/mlsql/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='mlsql',
+    version='0.1',
+    description='SQL like language for machine learning applications',
+    url='https://github.com/UI-DataScience/ML-SQL',
+    author='Neeraj Asthana',
+    author_email='neeasthana@gmail.com',
+    license='UIUC',
+    packages=['mlsql'],
+    install_requires=['markdown',],
+    zip_safe=False)
