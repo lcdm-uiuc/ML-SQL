77a8f97e9b24b989e98f88320c21c47876143387
Michael Hao <mxhao2@illinois.edu>
2016-07-04 23:38:26 -0400

    fix some issues on encoding functions with python3

--- language/mlsql/mlsql/functions/keywords/preprocessing/impute_functions.py
+++ language/mlsql/mlsql/functions/keywords/preprocessing/impute_functions.py
@@ -7,6 +7,11 @@ from sklearn.base import BaseEstimator, TransformerMixin
 # Usage:
 # imputer = ImputeColumns(impute_strategy, missing_vals)
 # imputer.fit_transform(data)
+# impute_strategy:
+#   “mean”: replace missing values using the mean along the axis.
+#   “median”: replace missing values using the median along the axis.
+#   “most_frequent”: replace missing using the most frequent value along the axis.
+
 class ImputeColumns(BaseEstimator, TransformerMixin):
 
   # Constructor: Makes an instance of the custom imputer
@@ -24,15 +29,17 @@ class ImputeColumns(BaseEstimator, TransformerMixin):
       self.imputer = Imputer(missing_values= self.missing_vals, strategy=self.impute_strategy)
       self.imputer.fit(data[self.columns])
       return self
+
   def transform(self, data):
       output = data.copy()
       output[self.columns] = self.imputer.transform(output[self.columns])
       return output
 
-# Makes an instance of the imputer class and returns an imputed dataframe
+# If the strategy is 
 def impute_missing(data, columns, impute_strategy='most_frequent', missing_values='NaN'):
-  imputer = ImputeColumns(columns=columns, impute_strategy=impute_strategy, missing_vals=missing_values)
-  return imputer.fit_transform(data)
+  if impute_strategy == 'most_frequent' or impute_strategy == 'median' or impute_strategy == 'mean':
+    imputer = ImputeColumns(columns=columns, impute_strategy=impute_strategy, missing_vals=missing_values)
+    return imputer.fit_transform(data)
 
 def removeColumns(data, delete_list):
   for col in delete_list:
