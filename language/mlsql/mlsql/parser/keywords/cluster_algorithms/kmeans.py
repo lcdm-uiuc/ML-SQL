f3bf56e21e606d997d39d5cb0decdead6e2da23a
Neeraj Asthana <nasthan2@illinois.edu>
2016-07-26 16:23:16 -0500

    finished documentation for classify, regress and cluster

--- language/mlsql/mlsql/parser/keywords/cluster_algorithms/kmeans.py
+++ language/mlsql/mlsql/parser/keywords/cluster_algorithms/kmeans.py
@@ -5,12 +5,12 @@ from pyparsing import oneOf, Literal, Optional, Word
 from ..grammer import numbers, openParen, closeParen
 
 def define_kmeans():
-    kmeansPhrase = oneOf(["k-means", "kmeans", "K-MEANS", "KMEANS"])
+    kmeansPhrase = oneOf(["k-means", "kmeans", "K-MEANS", "KMEANS", "KMeans", "K-Means"])
 
     #Definitions for options of kmeans (number of clusters)
     clusters_phrase = (Literal("clusters") + Literal("=")).suppress()
 
-    clusters = Optional(clusters_phrase + Word(numbers).setResultsName("clusters"), default = 1)
+    clusters = Optional(clusters_phrase + Word(numbers).setResultsName("clusters"), default = 3)
 
     #Compositions
     kmeans = kmeansPhrase + Optional(openParen + clusters + closeParen)
--- language/mlsql/mlsql/parser/keywords/regression_algorithms/ridge.py
+++ language/mlsql/mlsql/parser/keywords/regression_algorithms/ridge.py
@@ -1,12 +1,15 @@
 from pyparsing import oneOf, Literal, Optional, Word
-from ..grammer import numbers, openParen, closeParen
+from ..grammer import numbers, openParen, closeParen, decimal
 
 def define_ridge():
     ridgePhrase = oneOf(["ridge", "Ridge", "RIDGE"])
 
     #Options
+    lambda_phrase = (Literal("lambda") + Literal("=")).suppress()
+
+    lam = Optional(lambda_phrase + decimal.setResultsName("lambda"), default = 0)
 
     #Compositions
-    ridge = ridgePhrase + Optional(openParen + closeParen)
+    ridge = ridgePhrase + Optional(openParen + lam + closeParen)
 
     return ridge
