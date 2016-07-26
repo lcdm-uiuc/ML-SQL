e169a65cc97be79a1985ba4e7a22da67c1f98d79
Michael Hao <mxhao2@illinois.edu>
2016-07-26 14:55:47 -0400

    finish handle_replace, needs testing

--- language/mlsql/mlsql/functions/keywords/preprocessing/impute_functions.py
+++ language/mlsql/mlsql/functions/keywords/preprocessing/impute_functions.py
@@ -4,44 +4,6 @@ from sklearn.preprocessing import Imputer
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.ensemble import RandomForestRegressor
 
-
-"""
-Imputer Class
-Usage:
-imputer = ImputeColumns(impute_strategy, missing_vals)
-imputer.fit_transform(data)
-Parameters:
-  impute_strategy:
-    “mean”: replace missing values using the mean along the axis.
-    “median”: replace missing values using the median along the axis.
-    “most_frequent”: replace missing using the most frequent value along the axis.
-  missing_vals: Typically for Pandas, NaN is used for missing values
-
-"""
-class ImputeColumns(BaseEstimator, TransformerMixin):
-
-  # Constructor: Makes an instance of the custom imputer
-  # Columns: Columns to impute, if None, then impute all columns
-  # impute_strategy: Can be "mean", "median" or "most frequent"
-  def __init__(self,columns=None, impute_strategy='mode', missing_vals='NaN'):
-      self.columns = columns
-      self.impute_strategy = impute_strategy
-      if self.impute_strategy == 'mode':
-        self.impute_strategy = 'most_frequent'
-      self.missing_vals = missing_vals
-      self.imputer = None
-  def fit(self, data, target=None):
-      if self.columns is None:
-          self.columns = data.columns
-      self.imputer = Imputer(missing_values= self.missing_vals, strategy=self.impute_strategy)
-      self.imputer.fit(data[self.columns])
-      return self
-
-  def transform(self, data):
-      output = data.copy()
-      output[self.columns] = self.imputer.transform(output[self.columns])
-      return output
-
 """
 impute_missing()
 Parameters:
@@ -59,39 +21,40 @@ Parameters:
       'rand_forest_reg'
 Returns: Imputed dataframe
 """
-def impute_missing(data, columns, impute_strategy='mode', missing_values='NaN'):
+def impute_missing(data, columns=None, impute_strategy='mode', missing_values='NaN'):
+  datacopy = data
   dummy_val = 'U0'
   cols_to_impute = list()
   if columns == None:
-    cols_to_impute = _find_cols_with_missing_vals(data)
+    cols_to_impute = _find_cols_with_missing_vals(data, missing_values)
   else:
     cols_to_impute = columns
   if impute_strategy == 'mode':
     for col in cols_to_impute:
       modeVal = data[col].mode()
-      data[col].fillna(modeVal)
-    return data
+      datacopy[col] = data[col].fillna(modeVal[0])
+    return datacopy
   elif impute_strategy == 'mean':
     for col in cols_to_impute:
       meanVal = data[col].mean()
-      data[col].fillna(meanVal)
-    return data
+      datacopy[col] = data[col].fillna(meanVal)
+    return datacopy
   elif impute_strategy == 'median':
     for col in cols_to_impute:
       medianVal = data[col].median()
-      data[col].fillna(medianVal)
-    return data
+      datacopy[col] = data[col].fillna(medianVal)
+    return datacopy
   elif impute_strategy == 'drop column':
     return _remove_columns(data, cols_to_impute)
   elif impute_strategy == 'maximum':
     for col in cols_to_impute:
       maxVal = max(data[col])
-      data[col].fillna(maxVal)
+      datacopy[col] = data[col].fillna(maxVal)
     return data
   elif impute_strategy == 'minimum':
     for col in cols_to_impute:
       minVal = min(data[col])
-      data[col].fillna(minVal)
+      datacopy[col] = data[col].fillna(minVal)
     return data
   elif impute_strategy == 'dummy':
     return data.replace(missing_values, dummy_val) 
@@ -115,4 +78,17 @@ def _remove_columns(data, delete_list):
   for col in delete_list:
       del data[col]
   return data
+
+def _find_cols_with_missing_vals(data= None, missing_values= 'NaN'):
+  cols_to_impute = list()
+  if missing_values == 'NaN':
+    for col in f.columns:
+      if(data[col].isnull().values.any()):
+        cols_to_impute.append(col)
+  else:
+    for col in f.columns:
+      if missing_values in data[col]:
+        cols_to_impute.append(col)
+  return cols_to_impute
+
 
