a430b2be9500a5ba9f322f1f4804924bb97d1ff2
Kc Ikegwu <ikegwukc@gmail.com>
2016-08-25 11:51:52 -0500

    modified stdout to be more humanreadable

--- language/mlsql/mlsql/mlsql.py
+++ language/mlsql/mlsql/mlsql.py
@@ -3,19 +3,19 @@ import sys
 from .parser.parser import mlsqlparser
 from .functions.dataflow import handle
 
-def execute(command):
+def execute(command, verbose=False):
 	"""
 	Used to parse and execute an ML-SQL command supplied by the user. 
 	This function is meant to exported for use in other modules.
 	"""
 	myparser = mlsqlparser()
 	result = myparser.parseString(command)
-	print(result)
-	handle(result)
+	#print(result)
+	handle(result, verbose)
 
 
 
-def repl():
+def repl(verbose=False):
 	"""
 	Used to create a read-evaluate-print-loop sequence for the ML-SQL langauge.
 	This function will continually parse and execute user input from command
@@ -34,7 +34,7 @@ def repl():
 		else:
 			result = myparser.parseString(command)
 			print(result)
-			handle(result)
+			handle(result, verbose)
 
 if __name__ == "__main__":
 	repl()
